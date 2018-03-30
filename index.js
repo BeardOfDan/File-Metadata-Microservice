0579bd8ecbd8cd7e998597a98224e26666c6848c
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-30 14:42:19 -0700

    (feat) Add formattedSize
    Add a field to the resulting json with the size in 'Bytes', 'KB',
    'MB, or 'GB', it is called 'formattedSize'

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res, next) => {
   const filePath = path.join(__dirname + '/public/index.html');
-  res.sendFile(filePath)
+  res.sendFile(filePath);
 });
 
 app.post('/get-file-size', upload.single('file'), function (req, res, next) {
   const { originalname, size } = req.file;
   const filePath = req.file.path; // path is already a variable (the module), so not an ES6 declaration
 
+  // Delete the file, it's no longer needed
   fs.unlink(filePath, (err) => {
     if (err) {
       console.log(`ERROR! Failed to delete ${filePath}`)
@@ -24,10 +25,23 @@ app.post('/get-file-size', upload.single('file'), function (req, res, next) {
     // console.log(`successfully deleted ${filePath}`);
   });
 
-  // TODO: make a 'formattedSize' field
-  // This will use MB || KB to display the file size in an easily readable way
+  const labels = ['KB', 'MB', 'GB'];
 
-  return res.json({ 'File name': originalname, size });
+  let formattedSize = size;
+  let label = 'Bytes';
+
+  for (let i = 0; i < labels.length; i++) {
+    if (formattedSize < 1024) {
+      break;
+    }
+
+    formattedSize /= 1024;
+    label = labels[i];
+  }
+
+  formattedSize = formattedSize.toFixed(2) + ' ' + label;
+
+  return res.json({ 'File name': originalname, size, formattedSize });
 });
 
 app.listen(PORT, () => {
